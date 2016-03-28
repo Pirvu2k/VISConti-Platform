66124966f7888c0aed7a388f0301809858331f6e
Mihai Pirvulet <mitapirvuet@yahoo.com>
2016-03-28 21:14:39 +0300

    Dumped DB

--- visconti.sql
+++ visconti.sql
@@ -69,6 +69,266 @@ CREATE TABLE IF NOT EXISTS `administrator_roles` (
 /*!40000 ALTER TABLE `administrator_roles` ENABLE KEYS */;
 
 
+-- Dumping structure for table yii2basic.apps_countries
+CREATE TABLE IF NOT EXISTS `apps_countries` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `country_code` varchar(2) NOT NULL DEFAULT '',
+  `country_name` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=247 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table yii2basic.apps_countries: 246 rows
+/*!40000 ALTER TABLE `apps_countries` DISABLE KEYS */;
+INSERT INTO `apps_countries` (`id`, `country_code`, `country_name`) VALUES
+	(1, 'AF', 'Afghanistan'),
+	(2, 'AL', 'Albania'),
+	(3, 'DZ', 'Algeria'),
+	(4, 'DS', 'American Samoa'),
+	(5, 'AD', 'Andorra'),
+	(6, 'AO', 'Angola'),
+	(7, 'AI', 'Anguilla'),
+	(8, 'AQ', 'Antarctica'),
+	(9, 'AG', 'Antigua and Barbuda'),
+	(10, 'AR', 'Argentina'),
+	(11, 'AM', 'Armenia'),
+	(12, 'AW', 'Aruba'),
+	(13, 'AU', 'Australia'),
+	(14, 'AT', 'Austria'),
+	(15, 'AZ', 'Azerbaijan'),
+	(16, 'BS', 'Bahamas'),
+	(17, 'BH', 'Bahrain'),
+	(18, 'BD', 'Bangladesh'),
+	(19, 'BB', 'Barbados'),
+	(20, 'BY', 'Belarus'),
+	(21, 'BE', 'Belgium'),
+	(22, 'BZ', 'Belize'),
+	(23, 'BJ', 'Benin'),
+	(24, 'BM', 'Bermuda'),
+	(25, 'BT', 'Bhutan'),
+	(26, 'BO', 'Bolivia'),
+	(27, 'BA', 'Bosnia and Herzegovina'),
+	(28, 'BW', 'Botswana'),
+	(29, 'BV', 'Bouvet Island'),
+	(30, 'BR', 'Brazil'),
+	(31, 'IO', 'British Indian Ocean Territory'),
+	(32, 'BN', 'Brunei Darussalam'),
+	(33, 'BG', 'Bulgaria'),
+	(34, 'BF', 'Burkina Faso'),
+	(35, 'BI', 'Burundi'),
+	(36, 'KH', 'Cambodia'),
+	(37, 'CM', 'Cameroon'),
+	(38, 'CA', 'Canada'),
+	(39, 'CV', 'Cape Verde'),
+	(40, 'KY', 'Cayman Islands'),
+	(41, 'CF', 'Central African Republic'),
+	(42, 'TD', 'Chad'),
+	(43, 'CL', 'Chile'),
+	(44, 'CN', 'China'),
+	(45, 'CX', 'Christmas Island'),
+	(46, 'CC', 'Cocos (Keeling) Islands'),
+	(47, 'CO', 'Colombia'),
+	(48, 'KM', 'Comoros'),
+	(49, 'CG', 'Congo'),
+	(50, 'CK', 'Cook Islands'),
+	(51, 'CR', 'Costa Rica'),
+	(52, 'HR', 'Croatia (Hrvatska)'),
+	(53, 'CU', 'Cuba'),
+	(54, 'CY', 'Cyprus'),
+	(55, 'CZ', 'Czech Republic'),
+	(56, 'DK', 'Denmark'),
+	(57, 'DJ', 'Djibouti'),
+	(58, 'DM', 'Dominica'),
+	(59, 'DO', 'Dominican Republic'),
+	(60, 'TP', 'East Timor'),
+	(61, 'EC', 'Ecuador'),
+	(62, 'EG', 'Egypt'),
+	(63, 'SV', 'El Salvador'),
+	(64, 'GQ', 'Equatorial Guinea'),
+	(65, 'ER', 'Eritrea'),
+	(66, 'EE', 'Estonia'),
+	(67, 'ET', 'Ethiopia'),
+	(68, 'FK', 'Falkland Islands (Malvinas)'),
+	(69, 'FO', 'Faroe Islands'),
+	(70, 'FJ', 'Fiji'),
+	(71, 'FI', 'Finland'),
+	(72, 'FR', 'France'),
+	(73, 'FX', 'France, Metropolitan'),
+	(74, 'GF', 'French Guiana'),
+	(75, 'PF', 'French Polynesia'),
+	(76, 'TF', 'French Southern Territories'),
+	(77, 'GA', 'Gabon'),
+	(78, 'GM', 'Gambia'),
+	(79, 'GE', 'Georgia'),
+	(80, 'DE', 'Germany'),
+	(81, 'GH', 'Ghana'),
+	(82, 'GI', 'Gibraltar'),
+	(83, 'GK', 'Guernsey'),
+	(84, 'GR', 'Greece'),
+	(85, 'GL', 'Greenland'),
+	(86, 'GD', 'Grenada'),
+	(87, 'GP', 'Guadeloupe'),
+	(88, 'GU', 'Guam'),
+	(89, 'GT', 'Guatemala'),
+	(90, 'GN', 'Guinea'),
+	(91, 'GW', 'Guinea-Bissau'),
+	(92, 'GY', 'Guyana'),
+	(93, 'HT', 'Haiti'),
+	(94, 'HM', 'Heard and Mc Donald Islands'),
+	(95, 'HN', 'Honduras'),
+	(96, 'HK', 'Hong Kong'),
+	(97, 'HU', 'Hungary'),
+	(98, 'IS', 'Iceland'),
+	(99, 'IN', 'India'),
+	(100, 'IM', 'Isle of Man'),
+	(101, 'ID', 'Indonesia'),
+	(102, 'IR', 'Iran (Islamic Republic of)'),
+	(103, 'IQ', 'Iraq'),
+	(104, 'IE', 'Ireland'),
+	(105, 'IL', 'Israel'),
+	(106, 'IT', 'Italy'),
+	(107, 'CI', 'Ivory Coast'),
+	(108, 'JE', 'Jersey'),
+	(109, 'JM', 'Jamaica'),
+	(110, 'JP', 'Japan'),
+	(111, 'JO', 'Jordan'),
+	(112, 'KZ', 'Kazakhstan'),
+	(113, 'KE', 'Kenya'),
+	(114, 'KI', 'Kiribati'),
+	(115, 'KP', 'Korea, Democratic People\'s Republic of'),
+	(116, 'KR', 'Korea, Republic of'),
+	(117, 'XK', 'Kosovo'),
+	(118, 'KW', 'Kuwait'),
+	(119, 'KG', 'Kyrgyzstan'),
+	(120, 'LA', 'Lao People\'s Democratic Republic'),
+	(121, 'LV', 'Latvia'),
+	(122, 'LB', 'Lebanon'),
+	(123, 'LS', 'Lesotho'),
+	(124, 'LR', 'Liberia'),
+	(125, 'LY', 'Libyan Arab Jamahiriya'),
+	(126, 'LI', 'Liechtenstein'),
+	(127, 'LT', 'Lithuania'),
+	(128, 'LU', 'Luxembourg'),
+	(129, 'MO', 'Macau'),
+	(130, 'MK', 'Macedonia'),
+	(131, 'MG', 'Madagascar'),
+	(132, 'MW', 'Malawi'),
+	(133, 'MY', 'Malaysia'),
+	(134, 'MV', 'Maldives'),
+	(135, 'ML', 'Mali'),
+	(136, 'MT', 'Malta'),
+	(137, 'MH', 'Marshall Islands'),
+	(138, 'MQ', 'Martinique'),
+	(139, 'MR', 'Mauritania'),
+	(140, 'MU', 'Mauritius'),
+	(141, 'TY', 'Mayotte'),
+	(142, 'MX', 'Mexico'),
+	(143, 'FM', 'Micronesia, Federated States of'),
+	(144, 'MD', 'Moldova, Republic of'),
+	(145, 'MC', 'Monaco'),
+	(146, 'MN', 'Mongolia'),
+	(147, 'ME', 'Montenegro'),
+	(148, 'MS', 'Montserrat'),
+	(149, 'MA', 'Morocco'),
+	(150, 'MZ', 'Mozambique'),
+	(151, 'MM', 'Myanmar'),
+	(152, 'NA', 'Namibia'),
+	(153, 'NR', 'Nauru'),
+	(154, 'NP', 'Nepal'),
+	(155, 'NL', 'Netherlands'),
+	(156, 'AN', 'Netherlands Antilles'),
+	(157, 'NC', 'New Caledonia'),
+	(158, 'NZ', 'New Zealand'),
+	(159, 'NI', 'Nicaragua'),
+	(160, 'NE', 'Niger'),
+	(161, 'NG', 'Nigeria'),
+	(162, 'NU', 'Niue'),
+	(163, 'NF', 'Norfolk Island'),
+	(164, 'MP', 'Northern Mariana Islands'),
+	(165, 'NO', 'Norway'),
+	(166, 'OM', 'Oman'),
+	(167, 'PK', 'Pakistan'),
+	(168, 'PW', 'Palau'),
+	(169, 'PS', 'Palestine'),
+	(170, 'PA', 'Panama'),
+	(171, 'PG', 'Papua New Guinea'),
+	(172, 'PY', 'Paraguay'),
+	(173, 'PE', 'Peru'),
+	(174, 'PH', 'Philippines'),
+	(175, 'PN', 'Pitcairn'),
+	(176, 'PL', 'Poland'),
+	(177, 'PT', 'Portugal'),
+	(178, 'PR', 'Puerto Rico'),
+	(179, 'QA', 'Qatar'),
+	(180, 'RE', 'Reunion'),
+	(181, 'RO', 'Romania'),
+	(182, 'RU', 'Russian Federation'),
+	(183, 'RW', 'Rwanda'),
+	(184, 'KN', 'Saint Kitts and Nevis'),
+	(185, 'LC', 'Saint Lucia'),
+	(186, 'VC', 'Saint Vincent and the Grenadines'),
+	(187, 'WS', 'Samoa'),
+	(188, 'SM', 'San Marino'),
+	(189, 'ST', 'Sao Tome and Principe'),
+	(190, 'SA', 'Saudi Arabia'),
+	(191, 'SN', 'Senegal'),
+	(192, 'RS', 'Serbia'),
+	(193, 'SC', 'Seychelles'),
+	(194, 'SL', 'Sierra Leone'),
+	(195, 'SG', 'Singapore'),
+	(196, 'SK', 'Slovakia'),
+	(197, 'SI', 'Slovenia'),
+	(198, 'SB', 'Solomon Islands'),
+	(199, 'SO', 'Somalia'),
+	(200, 'ZA', 'South Africa'),
+	(201, 'GS', 'South Georgia South Sandwich Islands'),
+	(202, 'ES', 'Spain'),
+	(203, 'LK', 'Sri Lanka'),
+	(204, 'SH', 'St. Helena'),
+	(205, 'PM', 'St. Pierre and Miquelon'),
+	(206, 'SD', 'Sudan'),
+	(207, 'SR', 'Suriname'),
+	(208, 'SJ', 'Svalbard and Jan Mayen Islands'),
+	(209, 'SZ', 'Swaziland'),
+	(210, 'SE', 'Sweden'),
+	(211, 'CH', 'Switzerland'),
+	(212, 'SY', 'Syrian Arab Republic'),
+	(213, 'TW', 'Taiwan'),
+	(214, 'TJ', 'Tajikistan'),
+	(215, 'TZ', 'Tanzania, United Republic of'),
+	(216, 'TH', 'Thailand'),
+	(217, 'TG', 'Togo'),
+	(218, 'TK', 'Tokelau'),
+	(219, 'TO', 'Tonga'),
+	(220, 'TT', 'Trinidad and Tobago'),
+	(221, 'TN', 'Tunisia'),
+	(222, 'TR', 'Turkey'),
+	(223, 'TM', 'Turkmenistan'),
+	(224, 'TC', 'Turks and Caicos Islands'),
+	(225, 'TV', 'Tuvalu'),
+	(226, 'UG', 'Uganda'),
+	(227, 'UA', 'Ukraine'),
+	(228, 'AE', 'United Arab Emirates'),
+	(229, 'GB', 'United Kingdom'),
+	(230, 'US', 'United States'),
+	(231, 'UM', 'United States minor outlying islands'),
+	(232, 'UY', 'Uruguay'),
+	(233, 'UZ', 'Uzbekistan'),
+	(234, 'VU', 'Vanuatu'),
+	(235, 'VA', 'Vatican City State'),
+	(236, 'VE', 'Venezuela'),
+	(237, 'VN', 'Vietnam'),
+	(238, 'VG', 'Virgin Islands (British)'),
+	(239, 'VI', 'Virgin Islands (U.S.)'),
+	(240, 'WF', 'Wallis and Futuna Islands'),
+	(241, 'EH', 'Western Sahara'),
+	(242, 'YE', 'Yemen'),
+	(243, 'YU', 'Yugoslavia'),
+	(244, 'ZR', 'Zaire'),
+	(245, 'ZM', 'Zambia'),
+	(246, 'ZW', 'Zimbabwe');
+/*!40000 ALTER TABLE `apps_countries` ENABLE KEYS */;
+
+
 -- Dumping structure for table yii2basic.auth_assignment
 CREATE TABLE IF NOT EXISTS `auth_assignment` (
   `item_name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
@@ -153,12 +413,13 @@ CREATE TABLE IF NOT EXISTS `canvases` (
   `language` tinytext NOT NULL,
   `eng_summary` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
 
--- Dumping data for table yii2basic.canvases: ~1 rows (approximately)
+-- Dumping data for table yii2basic.canvases: ~0 rows (approximately)
 /*!40000 ALTER TABLE `canvases` DISABLE KEYS */;
 INSERT INTO `canvases` (`id`, `title`, `content`, `requested`, `student_id`, `created_by`, `date_added`, `date_modified`, `assigned_to`, `expert_id`, `language`, `eng_summary`) VALUES
-	(28, 'Project Example 1 ', '<h2><strong>Hello, my name is Mihai!</strong></h2>\r\n<p><strong>I am having a great time testing out VISConti!</strong></p>', '1', 5, 'test2', '2016-03-20 15:43:20', '2016-03-20 15:43:20', 'test3', 0, 'en', 'Short summary of the project, currently none :( ');
+	(28, 'Project Example 1 ', '<h2><strong>Hello, my name is Mihai!</strong></h2>\r\n<p><strong>I am having a great time testing out VISConti!</strong></p>', '1', 5, 'test2', '2016-03-20 15:43:20', '2016-03-20 15:43:20', 'test3', 0, 'en', 'Short summary of the project, currently none :( '),
+	(29, 'Test Canvas Skype (edit)', '<h1 style="text-align: center;">Title</h1>\r\n<p>content</p>\r\n<p>content</p>\r\n<p>content</p>', '1', 5, 'test2', '2016-03-22 19:46:50', '2016-03-22 19:47:13', '', 0, 'en', 'Test Summary. Hello World.');
 /*!40000 ALTER TABLE `canvases` ENABLE KEYS */;
 
 
@@ -567,7 +828,9 @@ INSERT INTO `migration` (`version`, `apply_time`) VALUES
 	('m141222_135246_alter_username_length', 1455032409),
 	('m150614_103145_update_social_account_table', 1455032413),
 	('m150623_212711_fix_username_notnull', 1455032415),
-	('m160211_135237_add_new_field_to_user', 1455201107);
+	('m160211_135237_add_new_field_to_user', 1455201107),
+	('m160327_111530_add_new_field_to_user', 1459078820),
+	('m160327_130443_add_new_field_to_user', 1459083973);
 /*!40000 ALTER TABLE `migration` ENABLE KEYS */;
 
 
@@ -576,26 +839,39 @@ CREATE TABLE IF NOT EXISTS `profile` (
   `user_id` int(11) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
   `public_email` varchar(255) DEFAULT NULL,
-  `gravatar_email` varchar(255) DEFAULT NULL,
-  `gravatar_id` varchar(32) DEFAULT NULL,
-  `location` varchar(255) DEFAULT NULL,
   `website` varchar(255) DEFAULT NULL,
   `bio` text,
+  `gravatar_email` varchar(50) DEFAULT NULL,
+  `state` varchar(50) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `zip` varchar(50) DEFAULT NULL,
+  `degree` varchar(50) DEFAULT NULL,
+  `degree_details` varchar(50) DEFAULT NULL,
+  `institution_name` varchar(50) DEFAULT NULL,
+  `start_year` varchar(50) DEFAULT NULL,
+  `end_year` varchar(50) DEFAULT NULL,
+  `byear` varchar(50) DEFAULT NULL,
+  `gravatar_id` varchar(50) DEFAULT NULL,
+  `phone_number` varchar(50) DEFAULT NULL,
+  `fax_number` varchar(50) DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`user_id`),
   CONSTRAINT `fk_user_profile` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table yii2basic.profile: ~8 rows (approximately)
+-- Dumping data for table yii2basic.profile: ~9 rows (approximately)
 /*!40000 ALTER TABLE `profile` DISABLE KEYS */;
-INSERT INTO `profile` (`user_id`, `name`, `public_email`, `gravatar_email`, `gravatar_id`, `location`, `website`, `bio`) VALUES
-	(3, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-	(4, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-	(5, '', '', '', 'd41d8cd98f00b204e9800998ecf8427e', '', '', ''),
-	(6, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-	(7, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-	(8, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-	(9, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-	(10, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `profile` (`user_id`, `name`, `public_email`, `website`, `bio`, `gravatar_email`, `state`, `address`, `city`, `zip`, `degree`, `degree_details`, `institution_name`, `start_year`, `end_year`, `byear`, `gravatar_id`, `phone_number`, `fax_number`, `country`) VALUES
+	(3, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	(4, 'da', '', '', '', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'd41d8cd98f00b204e9800998ecf8427e', '+40784198777', '+40784198777', '17'),
+	(5, 'Mihai The boss', 'test@test.com', '', 'AAAAAAAAA', NULL, '', 'yes', '', '21323', 'Master', 'I got this when i was 5', 'da', '1978', '2012', '1916', NULL, '+407222222222', '+407222222222', 'Albania'),
+	(6, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	(7, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	(8, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	(9, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	(10, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	(11, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 /*!40000 ALTER TABLE `profile` ENABLE KEYS */;
 
 
@@ -917,7 +1193,7 @@ CREATE TABLE IF NOT EXISTS `token` (
   CONSTRAINT `fk_user_token` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table yii2basic.token: ~8 rows (approximately)
+-- Dumping data for table yii2basic.token: ~9 rows (approximately)
 /*!40000 ALTER TABLE `token` DISABLE KEYS */;
 INSERT INTO `token` (`user_id`, `code`, `created_at`, `type`) VALUES
 	(3, 'tWxVq-0FBssj6IR5PtGpJ_k9nfDFm7V-', 1455036588, 0),
@@ -927,7 +1203,8 @@ INSERT INTO `token` (`user_id`, `code`, `created_at`, `type`) VALUES
 	(7, 'dH7qLfgdByzmmXSWgxI-TI8btUf9XuRM', 1456469418, 0),
 	(8, 'bTvWdYVMghKkF4uVxwZHBuBg3zqqvDej', 1456469487, 0),
 	(9, 'PXn3NAF0_HoyZXO8bCex84OMz18A8TQU', 1458467410, 0),
-	(10, 'xhDkO_z8pz9WbYy3BQxbGSP_8HqLBmih', 1458467644, 0);
+	(10, 'xhDkO_z8pz9WbYy3BQxbGSP_8HqLBmih', 1458467644, 0),
+	(11, 'x7G4jVQ3CFq38hkJnb6bBMJj2dPzfSC9', 1459083772, 0);
 /*!40000 ALTER TABLE `token` ENABLE KEYS */;
 
 
@@ -945,23 +1222,24 @@ CREATE TABLE IF NOT EXISTS `user` (
   `created_at` int(11) NOT NULL,
   `updated_at` int(11) NOT NULL,
   `flags` int(11) NOT NULL DEFAULT '0',
-  `field` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_unique_email` (`email`),
   UNIQUE KEY `user_unique_username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
--- Dumping data for table yii2basic.user: ~8 rows (approximately)
+-- Dumping data for table yii2basic.user: ~9 rows (approximately)
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` (`id`, `username`, `email`, `password_hash`, `auth_key`, `confirmed_at`, `unconfirmed_email`, `blocked_at`, `registration_ip`, `created_at`, `updated_at`, `flags`, `field`) VALUES
-	(3, 'test', 'mitapirvuet@yahoo.com', '$2y$12$X1gC0iAOF1/wq.37KK7a6.4ByI8YcAFlPgXVtPzvhM4lLg2OU.C6i', 'YNMdBsrnzqTBypz2F0jahEp24cyHRQx8', 1455200788, NULL, NULL, '::1', 1455036588, 1455036588, 0, NULL),
-	(4, 'admin', 'lalala@gmail.com', '$2y$12$Y6bhwDiSRuKCxMPQoQ3/Deo2FzxmLj.NW1XK6VsvGX7663Jz7v7xK', 'gI7PRVIdt8LOxLbcIup54pJRfixVyWb9', 1455200792, NULL, NULL, '::1', 1455198448, 1455198448, 0, NULL),
-	(5, 'test2', 'yes@yes.com', '$2y$12$/V84LAa.Y1c5QyIMd4azKeqOl45YOvQD7A9L7updqxVWNQmjpJlHu', 'USdLFxggBtb7DbL-DPfeLSA5hr_MOA4j', 1456469317, NULL, NULL, '::1', 1456469274, 1456469274, 0, NULL),
-	(6, 'test3', 'no@no.com', '$2y$12$bMJtQ6M68y3C/5ANzTqFN.Ie8JxWXKPwntAp3XXodVu5Ntk7Dk572', 'Lt9TIyxxJzgs4HSMULMpocShtyhd7uP3', 1456469968, NULL, NULL, '::1', 1456469374, 1456469374, 0, NULL),
-	(7, 'dada', 'da@da.com', '$2y$12$8TfJwH.94edrM9PqFUnMweqefHZl.FAlXdfJNRd9/8URq.ovI6OEm', '5nMqD6wQ2LSCDFLaSMHX9Nwdg_edQ1Uw', 1456469969, NULL, NULL, '::1', 1456469418, 1456469418, 0, NULL),
-	(8, 'testut', 'test@test.com', '$2y$12$Gb.jL6xRR1Per4EPrwpXNeSUQ/4wJ4Uxws1KBlmXc6o8EY4f1C/GK', 'bvjkeSKKiSzjARuWt-9FINWQLiCCvG6O', 1456469971, NULL, NULL, '::1', 1456469487, 1456469487, 0, NULL),
-	(9, 'student', 'test@testtest.com', '$2y$12$rDTteIiZxR0Kp5fSbCo50.emk0IO5G9bvZtIup.J1iMuFd8nCEcPu', 'ymU-B19TONillZaOzHP4k0JAUM7lzzwY', NULL, NULL, NULL, '::1', 1458467409, 1458467409, 0, NULL),
-	(10, 'student2', 'test@testtests.com', '$2y$12$b6GUl/mfmfofmUnd00E/geKsXGJ/zi9kSuSMIfc3Qj.51fMmOYSDG', 'WQuC69jD2EsU37JIfyM1F1JDTpZGdH-y', NULL, NULL, NULL, '::1', 1458467643, 1458467643, 0, NULL);
+INSERT INTO `user` (`id`, `username`, `email`, `password_hash`, `auth_key`, `confirmed_at`, `unconfirmed_email`, `blocked_at`, `registration_ip`, `created_at`, `updated_at`, `flags`, `phone_number`) VALUES
+	(3, 'test', 'mitapirvuet@yahoo.com', '$2y$12$X1gC0iAOF1/wq.37KK7a6.4ByI8YcAFlPgXVtPzvhM4lLg2OU.C6i', 'YNMdBsrnzqTBypz2F0jahEp24cyHRQx8', 1455200788, NULL, NULL, '::1', 1455036588, 1455036588, 0, ''),
+	(4, 'admin', 'lalala@gmail.com', '$2y$12$Y6bhwDiSRuKCxMPQoQ3/Deo2FzxmLj.NW1XK6VsvGX7663Jz7v7xK', 'gI7PRVIdt8LOxLbcIup54pJRfixVyWb9', 1455200792, NULL, NULL, '::1', 1455198448, 1455198448, 0, ''),
+	(5, 'test2', 'yes@yes.com', '$2y$12$/V84LAa.Y1c5QyIMd4azKeqOl45YOvQD7A9L7updqxVWNQmjpJlHu', 'USdLFxggBtb7DbL-DPfeLSA5hr_MOA4j', 1456469317, NULL, NULL, '::1', 1456469274, 1456469274, 0, ''),
+	(6, 'test3', 'no@no.com', '$2y$12$bMJtQ6M68y3C/5ANzTqFN.Ie8JxWXKPwntAp3XXodVu5Ntk7Dk572', 'Lt9TIyxxJzgs4HSMULMpocShtyhd7uP3', 1456469968, NULL, NULL, '::1', 1456469374, 1456469374, 0, ''),
+	(7, 'dada', 'da@da.com', '$2y$12$8TfJwH.94edrM9PqFUnMweqefHZl.FAlXdfJNRd9/8URq.ovI6OEm', '5nMqD6wQ2LSCDFLaSMHX9Nwdg_edQ1Uw', 1456469969, NULL, NULL, '::1', 1456469418, 1456469418, 0, ''),
+	(8, 'testut', 'test@test.com', '$2y$12$Gb.jL6xRR1Per4EPrwpXNeSUQ/4wJ4Uxws1KBlmXc6o8EY4f1C/GK', 'bvjkeSKKiSzjARuWt-9FINWQLiCCvG6O', 1456469971, NULL, NULL, '::1', 1456469487, 1456469487, 0, ''),
+	(9, 'student', 'test@testtest.com', '$2y$12$rDTteIiZxR0Kp5fSbCo50.emk0IO5G9bvZtIup.J1iMuFd8nCEcPu', 'ymU-B19TONillZaOzHP4k0JAUM7lzzwY', NULL, NULL, NULL, '::1', 1458467409, 1458467409, 0, ''),
+	(10, 'student2', 'test@testtests.com', '$2y$12$b6GUl/mfmfofmUnd00E/geKsXGJ/zi9kSuSMIfc3Qj.51fMmOYSDG', 'WQuC69jD2EsU37JIfyM1F1JDTpZGdH-y', NULL, NULL, NULL, '::1', 1458467643, 1458467643, 0, ''),
+	(11, 'testfield', 'cicatest@test.com', '$2y$12$bR6APZDU946P7m6y1hQEkehd3DxGcmBWMfpjw/QaOkmfwBC0FBZbm', 'rEjCnIpNU9871WnnXv21eOK7jKHl0mH_', NULL, NULL, NULL, '::1', 1459083771, 1459083771, 0, '');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
