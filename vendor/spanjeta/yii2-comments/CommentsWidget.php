c9579b21655241726d9f05fe4c86b60466b84d15
Ionut Popescu <ionutpopescu10@yahoo.com>
2016-04-25 22:26:25 +0300

    Implemented comments at canvases.

---
+++ vendor/spanjeta/yii2-comments/CommentsWidget.php
@@ -0,0 +1,56 @@
+<?php
+namespace spanjeta\comments;
+
+use spanjeta\comments\models\Comment;
+use yii\data\ActiveDataProvider;
+
+class CommentsWidget extends \yii\base\Widget
+{
+
+    public $disabled = false;
+
+    public $model;
+
+    public $readOnly = false;
+
+    protected function getRecentComments()
+    {
+        if ($this->model == null)
+            return null;
+		
+		$query = Comment::find()->where(['model_id' => $_GET["id"]]);
+
+        return new ActiveDataProvider(['query' =>$query]);
+    }
+
+    protected function formModel()
+    {
+        $comment = null;
+        if ($this->readOnly == false) {
+            $comment = new Comment();
+            $comment->model_type = get_class($this->model);
+            $comment->model_id = $this->model->id;
+        }
+        return $comment;
+    }
+
+    public function run()
+    {
+        if ($this->disabled)
+            return; //Do nothing
+        
+        if (isset($_POST['Comment'])) {
+            $comment = new Comment();
+            $comment->comment = $_POST['Comment']['comment'];
+            $comment->model_type = get_class($this->model);
+            $comment->model_id = $this->model->id;
+
+            $comment->save();
+        }
+        
+        echo $this->render('comments', [
+            'comments' => $this->getRecentComments(),
+            'model' => $this->formModel()
+        ]);
+    }
+}
