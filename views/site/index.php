53bf9d43143387e75ffc19e44677b802a8068bbd
IonutPopescuRO <ionutpopescu10@yahoo.com>
2016-04-29 18:07:08 +0300

    Design optimization

--- views/site/index.php
+++ views/site/index.php
@@ -7,6 +7,25 @@ $this->title = 'Visconti';
 
 ?>
 
+<div class="row">
+    <h2> Recent Activities </h2>
+    <ul class="col-md-10 col-sm-9 feed">
+        <?php foreach($activities as $activity): ?>
+        <li>
+            <h2 class="col-sm-12"><a href="?r=canvas/view&id=<?= $activity->canvas ?> "><?= $activity->action_type ?></a></h2>						<span class="pull-right"><time><?= $activity->created_on ?></time></span>
+            <div class="col-sm-12 clearfix">
+                <data><b><?= $activity->activity_text ?></b>
+                </data>
+            </div>
+            <div class="clearfix visible-*"></div>
+        </li>
+        <?php endforeach; 
+        if(empty($activities))
+            echo "<p> Nothing here. </p>";
+        ?>
+    </ul>
+</div>
+
 <?php if(!Yii::$app->user->isGuest && Yii::$app->user->identity->type == 'e') {?>
 
 <?php
@@ -23,12 +42,11 @@ $this->title = 'Visconti';
     <ul class="col-md-10 col-sm-9 feed">
         <?php foreach($acceptedProjects as $project): ?>
         <li>
-            <h2 class="col-sm-12"><a href="?r=canvas/view&id=<?= $project->getProject()->id ?> "><?= $project->getProject()->title ?></a></h2>
+            <h2 class="col-sm-12"><a href="?r=canvas/view&id=<?= $project->getProject()->id ?> "><?= $project->getProject()->title ?></a></h2>						<span class="pull-right"><time><?= $project->getProject()->date_added ?></time></span>
             <div class="col-sm-12 clearfix">
                 <data><b><?= $project->getProject()->eng_summary ?></b>
                 </data>
             </div>
-            <time><?= $project->getProject()->date_added ?></a></time>
             <div class="clearfix visible-*"></div>
         </li>
         <?php endforeach; 
@@ -46,12 +64,11 @@ $this->title = 'Visconti';
     <ul class="col-md-10 col-sm-9 feed">
         <?php foreach($invitations as $invitation): ?>
         <li>
-            <h2 class="col-sm-12"><a href="?r=canvas/view&id=<?= $invitation->getProject()->id ?> "><?= $invitation->getProject()->title ?></a></h2>
+            <h2 class="col-sm-12"><a href="?r=canvas/view&id=<?= $invitation->getProject()->id ?> "><?= $invitation->getProject()->title ?></a></h2>						<span class="pull-right"><time><?= $invitation->getProject()->date_added ?></time></span>
             <div class="col-sm-12 clearfix">
                 <data><b><?= $invitation->getProject()->eng_summary ?></b>
                 </data>
             </div>
-            <time><?= $invitation->getProject()->date_added ?></a></time>
             <div class="clearfix visible-*"></div>
         </li>
         <?php endforeach; 
@@ -69,12 +86,11 @@ $this->title = 'Visconti';
     <ul class="col-md-10 col-sm-9 feed">
         <?php foreach($studentAcceptedProjects as $project): ?>
         <li>
-            <h2 class="col-sm-12"><a href="?r=canvas/view&id=<?= $project->id ?> "><?= $project->title ?></a></h2>
+            <h2 class="col-sm-12"><a href="?r=canvas/view&id=<?= $project->id ?> "><?= $project->title ?></a></h2>						<span class="pull-right"><time><?= $project->date_added ?></time></span>
             <div class="col-sm-12 clearfix">
                 <data><b><?= $project->eng_summary ?></b>
                 </data>
             </div>
-            <time><?= $project->date_added ?></a></time>
             <div class="clearfix visible-*"></div>
         </li>
         <?php endforeach; 
@@ -92,12 +108,11 @@ $this->title = 'Visconti';
     <ul class="col-md-10 col-sm-9 feed">
         <?php foreach($ownProjects as $project): ?>
         <li>
-            <h2 class="col-sm-12"><a href="?r=canvas/view&id=<?= $project->id ?> "><?= $project->title ?></a></h2>
+            <h2 class="col-sm-12"><a href="?r=canvas/view&id=<?= $project->id ?> "><?= $project->title ?></a></h2>						<span class="pull-right"><time><?= $project->date_added ?></time></span>
             <div class="col-sm-12 clearfix">
                 <data><b><?= $project->eng_summary ?></b>
                 </data>
             </div>
-            <time><?= $project->date_added ?></a></time>
             <div class="clearfix visible-*"></div>
         </li>
         <?php endforeach; 
